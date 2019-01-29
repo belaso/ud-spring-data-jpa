b4388e3b1705cc0baaf96f89ad08aaed8a58be78
Christian Trutz <info@christian-trutz.de>
2019-01-29 08:44:36 +0100

    Logging eingeschaltet und findAll Methode

--- src/main/java/com/example/demo/DemoApplicationRunner.java
+++ src/main/java/com/example/demo/DemoApplicationRunner.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -11,15 +13,23 @@ import com.example.demo.person.PersonRepository;
 @Component
 public class DemoApplicationRunner implements ApplicationRunner {
 
+	private final static Logger logger = LoggerFactory.getLogger(DemoApplicationRunner.class);
+
 	@Autowired
 	private PersonRepository personRepository;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+
+		// Entity anlegen
 		Person person = new Person();
 		person.setVorname("Christian");
 		person.setNachname("Trutz");
 		personRepository.save(person);
+		logger.info("Entity angelegt.");
+
+		// Entity finden
+		logger.info("Anzahl gefundene Objekte: {}.", personRepository.findAll().size());
 	}
 
 }
