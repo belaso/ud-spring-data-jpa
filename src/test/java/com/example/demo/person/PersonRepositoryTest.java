e35823aa0f5e651d89f21d6bd90f4ed979dcc5ea
Christian Trutz <info@christian-trutz.de>
2019-02-19 08:25:15 +0100

    Test mit Transaktionen wiederbelebt

--- src/test/java/com/example/demo/person/PersonRepositoryTest.java
+++ src/test/java/com/example/demo/person/PersonRepositoryTest.java
@@ -2,18 +2,26 @@ package com.example.demo.person;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
 
+import javax.transaction.Transactional;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.transaction.TestTransaction;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonRepositoryTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(PersonRepositoryTest.class);
 
 	@Autowired
 	private PersonRepository personRepository;
@@ -34,6 +42,51 @@ public class PersonRepositoryTest {
 		johndoe.setName("John Doe");
 		// save bedeutet hier ein SQL update
 		personRepository.save(johndoe);
+
+	}
+
+	@Test
+	@Transactional
+	public void testParent() throws SQLException {
+
+		// Transaktion: Adam und Kain werden gespeichert
+		{
+			assertTrue(TestTransaction.isActive());
+
+			// Adam
+			Person adam = new Person();
+			adam.setName("Adam");
+			personRepository.save(adam);
+
+			// Kain
+			for (int i = 1; i <= 10; i++) {
+				Person kain = new Person();
+				kain.setName("Kain " + i);
+				kain.setParent(adam);
+				personRepository.save(kain);
+			}
+
+			TestTransaction.flagForCommit();
+			TestTransaction.end();
+		}
+
+		// Transaktion: Person#children werden gelesen
+		{
+			TestTransaction.start();
+
+			Long idAdam = 1L;
+			personRepository.findById(idAdam).ifPresent(entity -> {
+				entity.getChildren().forEach(child -> {
+					assertNotNull(child);
+					logger.info("Child named {} with parent {}.", child.getName(), child.getParent().getName());
+				});
+			});
+
+			TestTransaction.flagForCommit();
+			TestTransaction.end();
+		}
+
+		// Server.startWebServer(datasource.getConnection());
 
 	}
 
